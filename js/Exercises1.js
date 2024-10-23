b5ee753130e46a1b1f8c1f33ec2e20f5116100e4
DanielChan20 <2302152@utrivieramaya.edu.mx>
2024-10-23 08:38:50 -0500

    first commit

---
+++ js/Exercises1.js
@@ -0,0 +1,3 @@
+//los comentarios pueden hacer que el codigo sea legible.
+//Este es mi primer codigo JS.
+/*los comentarios pueden hacer que el codigo sea legible, facil de reutilizar e informativo*/
---
+++ js/variable.js
@@ -0,0 +1,34 @@
+//declare variables y asigne tipos de datos de cadena, booleanos, indefinidos y nulos.
+
+let nombre = 'karla';//string
+let status = false; //boolean
+let city; //undefined
+let = address; //null
+
+
+let materia1;
+let materia2;
+let materia3;
+let materia4;
+
+let profesor = 'Prof. Pino';
+let profesor1 = 'Prof. Perla';
+let profesor2 = 'Prof. Geovanni';
+let profesor3 = 'Prof. Albino';
+
+let myName = 'Daniel';
+let myLastName = 'Chan';
+let isMarried = false;
+let country = 'Mexico';
+let age = 21;
+
+let myName1, myLastName1, isMarried1, country1, age1;
+
+name1 = 'Adrian';
+name1 = 'Cecilia';
+name1 = 'Sebastian';
+
+let myAge = 21;
+let yourAge = 25;
+
+console.log('Mi edad es' + '' + myAge, 'Tu edad es' + '' + yourAge);
